999363988ea7a7ed05cf030471ec7b9814983870
Anis Jamadar <anisjamadar26@gmail.com>
2023-03-08 20:34:21 +0530

    FEATURE : SWIPE TO SHOW DELETE ICON

--- app/src/main/java/com/example/mynewsapp/presentation/view/SavedNewsFragment.kt
+++ app/src/main/java/com/example/mynewsapp/presentation/view/SavedNewsFragment.kt
@@ -1,10 +1,18 @@
 package com.example.mynewsapp.presentation.view
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.ItemTouchHelper
@@ -15,6 +23,7 @@ import com.example.mynewsapp.data.utils.VariableConstants
 import com.example.mynewsapp.databinding.FragmentSavedNewsBinding
 import com.example.mynewsapp.presentation.adapter.NewsHeadLineAdapter
 import com.example.mynewsapp.presentation.viewmodel.NewsViewModel
+import com.example.mynewsapp.swipe.SwipeHelper
 import com.google.android.material.snackbar.Snackbar
 
 class SavedNewsFragment : Fragment() {
@@ -45,7 +54,8 @@ class SavedNewsFragment : Fragment() {
             )
         }
         initRecyclerview()
-        val itemTouchHelperCallback = object : ItemTouchHelper.SimpleCallback(
+        setUpRecyclerView()
+        /*val itemTouchHelperCallback = object : ItemTouchHelper.SimpleCallback(
             ItemTouchHelper.UP or ItemTouchHelper.DOWN,
             ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
         ) {
@@ -58,7 +68,7 @@ class SavedNewsFragment : Fragment() {
             }
 
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                val position  = viewHolder.adapterPosition
+                /*val position  = viewHolder.adapterPosition
                 val article = newsAdapter.differ.currentList[position]
                 viewModel.deleteSavedNews(article)
                 Snackbar.make(view, "Deleted", Snackbar.LENGTH_LONG).apply {
@@ -66,22 +76,106 @@ class SavedNewsFragment : Fragment() {
                         viewModel.saveArticle(article)
                     }
                     show()
+                }*/
+            }
+
+            override fun onChildDraw(
+                c: Canvas,
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                dx: Float,
+                dy: Float,
+                actionState: Int,
+                isCurrentlyActive: Boolean
+            ) {
+                var icon: Bitmap
+                if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+                    val itemView = viewHolder.itemView
+                    val height = itemView.bottom.toFloat() - itemView.top.toFloat()
+                    val width = height / 3
+
+                    if (dx > 0) {
+                        var p: Paint = Paint()
+                        p.setColor(Color.parseColor("#388E3C"))
+                        val background = RectF(itemView.left.toFloat(), itemView.top.toFloat(), dx, itemView.bottom.toFloat())
+                        c.drawRect(background, p)
+                        icon = context?.let { ContextCompat.getDrawable(it, R.drawable.ic_android_black_24dp)?.toBitmap() }!!
+                        val icon_dest = RectF(itemView.left.toFloat() + width, itemView.top.toFloat() + width, itemView.left + 2 * width, itemView.bottom.toFloat() - width)
+                        c.drawBitmap(icon, null, icon_dest, p)
+                    } else if (dx < 0) {
+                        var p: Paint = Paint()
+                        p.setColor(Color.parseColor("#D32F2F"))
+                        val background = RectF(itemView.right.toFloat() + dx, itemView.top.toFloat(), itemView.right.toFloat(), itemView.bottom.toFloat())
+                        c.drawRect(background, p)
+                        icon = context?.let { ContextCompat.getDrawable(it, R.drawable.ic_android_black_24dp)?.toBitmap() }!!
+                        val icon_dest = RectF(itemView.left + 3 * width, itemView.top.toFloat() + width, itemView.right.toFloat() + 1, itemView.bottom.toFloat() - width)
+                        c.drawBitmap(icon, null, icon_dest, p)
+                    }
                 }
+
+                super.onChildDraw(
+                    c,
+                    recyclerView,
+                    viewHolder,
+                    dx / 4,
+                    dy,
+                    actionState,
+                    isCurrentlyActive
+                )
             }
 
         }
         ItemTouchHelper(itemTouchHelperCallback).apply {
             attachToRecyclerView(binding.rvSavedNews)
-        }
+        }*/
         viewModel.getSavedNews().observe(viewLifecycleOwner, Observer {
             newsAdapter.differ.submitList(it)
         })
     }
+
 
     private fun initRecyclerview() {
         binding.rvSavedNews.apply {
             adapter = newsAdapter
             layoutManager = LinearLayoutManager(activity)
         }
+    }
+
+    private fun setUpRecyclerView() {
+
+        val itemTouchHelper = ItemTouchHelper(object : SwipeHelper(binding.rvSavedNews) {
+            override fun instantiateUnderlayButton(position: Int): List<UnderlayButton> {
+                val deleteButton = deleteButton(position)
+                return listOf(deleteButton)
+            }
+        })
+
+        itemTouchHelper.attachToRecyclerView(binding.rvSavedNews)
+    }
+
+    private fun toast(text: String) {
+
+    }
+
+    private fun deleteButton(position: Int) : SwipeHelper.UnderlayButton {
+        return SwipeHelper.UnderlayButton(
+            context!!,
+            "Delete",
+            14.0f,
+            android.R.color.holo_red_light,
+            object : SwipeHelper.UnderlayButtonClickListener {
+                override fun onClick() {
+                    val article = newsAdapter.differ.currentList[position]
+                    viewModel.deleteSavedNews(article)
+                    view?.let {
+                        Snackbar.make(it, "Deleted", Snackbar.LENGTH_LONG).apply {
+                            setAction("Undo") {
+                                viewModel.saveArticle(article)
+                            }
+                            show()
+                        }
+                    }
+                }
+            })
     }
 }
